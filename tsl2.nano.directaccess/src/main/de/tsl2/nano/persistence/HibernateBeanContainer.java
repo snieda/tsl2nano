4114f34649443144dd4994056981d47c45461d8a
snieda <snieda@web.de>
2013-07-15 09:53:06 +0200

    New: services/beancontainer for file-serialization and configuration

--- tsl2.nano.directaccess/src/main/de/tsl2/nano/persistence/HibernateBeanContainer.java
+++ tsl2.nano.directaccess/src/main/de/tsl2/nano/persistence/HibernateBeanContainer.java
@@ -34,11 +34,9 @@ import de.tsl2.nano.util.bean.BeanContainer;
  * @version $Revision$
  */
 @SuppressWarnings({"rawtypes", "unchecked", "serial"})
-public class HibernateBeanContainer extends BeanContainerUtil {
+public class HibernateBeanContainer extends GenericBeanContainer {
     protected Map properties = new HashMap();
     protected IGenericService service;
-
-    private static final Log LOG = LogFactory.getLog(HibernateBeanContainer.class);
 
     /**
      * initializes the standard bean container to use GenericService methods. it creates an own servicefactory using the
@@ -47,101 +45,7 @@ public class HibernateBeanContainer extends BeanContainerUtil {
      * @param classloader loader to be used inside the own servicefactory instance.
      */
     public static void initHibernateContainer(ClassLoader classloader) {
-        final HibernateBeanContainer hibContainer = new HibernateBeanContainer();
-
-        IAction<Collection<?>> typeFinder = new CommonAction<Collection<?>>() {
-            @Override
-            public Collection<?> action() {
-                Class entityType = (Class) parameter[0];
-                int maxResult = (Integer) parameter[1];
-                if (!new BeanClass(entityType).isAnnotationPresent(Entity.class)) {
-                    return null;
-                }
-                return hibContainer.getService().findAll(entityType, maxResult);
-            }
-        };
-        IAction<Collection<?>> exampleFinder = new CommonAction<Collection<?>>() {
-            @Override
-            public Collection<?> action() {
-                return hibContainer.getService().findByExample(parameter[0], true);
-            }
-        };
-        IAction<Collection<?>> betweenFinder = new CommonAction<Collection<?>>() {
-            @Override
-            public Collection<?> action() {
-                return hibContainer.getService().findBetween(parameter[0], parameter[1], true, (Integer) parameter[2]);
-            }
-        };
-        IAction<Collection<?>> queryFinder = new CommonAction<Collection<?>>() {
-            @Override
-            public Collection<?> action() {
-                return hibContainer.getService().findByQuery((String) parameter[0],
-                    (Boolean) parameter[1],
-                    (Object[]) parameter[2],
-                    (Class[]) parameter[3]);
-            }
-        };
-        IAction lazyrelationResolver = new CommonAction() {
-            @Override
-            public Object action() {
-                //use the weak implementation of BeanClass to avoid classloader problems!
-                if (new BeanClass(parameter[0].getClass()).isAnnotationPresent(Entity.class)) {
-                    return hibContainer.getService().instantiateLazyRelationship(parameter[0]);
-                } else {
-                    return parameter[0];
-                }
-            }
-        };
-        IAction saveAction = new CommonAction() {
-            @Override
-            public Object action() {
-                return hibContainer.getService().persist(parameter[0]);
-            }
-        };
-        IAction deleteAction = new CommonAction() {
-            @Override
-            public Object action() {
-                hibContainer.getService().remove(parameter[0]);
-                return null;
-            }
-        };
-        IAction attrAction = new CommonAction() {
-            @Override
-            public Object action() {
-                return getAttributeDefinitions(parameter[0], (String) parameter[1]);
-            }
-        };
-        IAction permissionAction = new CommonAction() {
-            @Override
-            public Object action() {
-                return true;//BeanContainer.hasPermission(beanType, packedInList, (String) parameter[0]);
-            }
-        };
-        IAction persistableAction = new CommonAction() {
-            @Override
-            public Object action() {
-                return BeanContainerUtil.isPersistable((Class<?>) parameter[0]);
-            }
-        };
-        final IAction<Integer> executeAction = new CommonAction<Integer>() {
-            @Override
-            public Integer action() {
-                return hibContainer.getService().executeQuery((String) parameter[0],
-                    (Boolean) parameter[1],
-                    (Object[]) parameter[2]);
-            }
-        };
-        BeanContainer.initServiceActions(typeFinder,
-            lazyrelationResolver,
-            saveAction,
-            deleteAction,
-            exampleFinder,
-            betweenFinder,
-            queryFinder,
-            attrAction,
-            permissionAction,
-            persistableAction,
-            executeAction);
+        initContainer(new HibernateBeanContainer(), classloader);
     }
 
     protected IGenericService getService() {
@@ -203,15 +107,4 @@ public class HibernateBeanContainer extends BeanContainerUtil {
 //        return session;
 //    }
 
-    public Object get(Object key) {
-        return properties.get(key);
-    }
-
-    protected void put(Object key, Object value) {
-        properties.put(key, value);
-    }
-
-    protected Object remove(Object key) {
-        return properties.remove(key);
-    }
 }
