7e25242f6f4e5c59e92b0147cac04bcece3df6d2
snieda <snieda@web.de>
2013-07-18 08:22:58 +0200

    default formatting extended xml serialization of formats correction on filteringiterator table header sorting

---
+++ tsl2.nano.common/src/main/de/tsl2/nano/format/INumberFormatCheck.java
@@ -0,0 +1,19 @@
+/*
+ * File: $HeadURL$
+ * Id  : $Id$
+ * 
+ * created by: ts
+ * created on: 17.07.2013
+ * 
+ * Copyright: (c) Thomas Schneider 2013, all rights reserved
+ */
+package de.tsl2.nano.format;
+
+/**
+ * provides the check to be a number format - used for styling like alignment
+ * @author ts
+ * @version $Revision$ 
+ */
+public interface INumberFormatCheck {
+    boolean isNumber();
+}
