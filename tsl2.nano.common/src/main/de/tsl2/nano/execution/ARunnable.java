f5ac17ede6aff28d1a764bb7259e8944f3341cc4
Thomas Schneider <snieda@web.de>
2013-11-30 12:55:08 +0100

    New: combination of IRunnable and Runnable

---
+++ tsl2.nano.common/src/main/de/tsl2/nano/execution/ARunnable.java
@@ -0,0 +1,49 @@
+/*
+ * File: $HeadURL$
+ * Id  : $Id$
+ * 
+ * created by: Tom
+ * created on: 29.11.2013
+ * 
+ * Copyright: (c) Thomas Schneider 2013, all rights reserved
+ */
+package de.tsl2.nano.execution;
+
+import java.io.Serializable;
+
+/**
+ * combines the two interfaces {@link IRunnable} and {@link Runnable} to implement {@link IRunnable}s usable by
+ * {@link Thread}s.
+ * 
+ * @author Tom
+ * @version $Revision$
+ */
+public abstract class ARunnable<CONTEXT extends Serializable> implements IRunnable<CONTEXT>, Runnable {
+    CONTEXT context;
+    Object[] args;
+    
+    /**
+     * constructor
+     * @param context
+     * @param args
+     */
+    public ARunnable(CONTEXT context, Object[] args) {
+        super();
+        this.context = context;
+        this.args = args;
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void run() {
+        context = run(context, args);
+    }
+    public void setArgs(Object[] args) {
+        this.args = args;
+    }
+    public CONTEXT getResult() {
+        return context;
+    }
+}
--- tsl2.nano.common/src/main/de/tsl2/nano/execution/IRunnable.java
+++ tsl2.nano.common/src/main/de/tsl2/nano/execution/IRunnable.java
@@ -13,8 +13,10 @@ import java.io.Serializable;
 
 /**
  * generic interface to start any runnable with a given context and returning the context as result. the standard
- * {@link Runnable} interface has no args. the extArgs argument is optional extending of the caller arguements.</p>
- * useful for stateless remote calls - then the context must be serializable.
+ * {@link Runnable} interface has no args and no return value. the extArgs argument is optional extending of the caller
+ * arguments.</p> useful for stateless remote calls - then the context must be serializable. like the {@link Runnable}
+ * this interface does not throw a checked exception. checked exceptions should be handled by catching them and throwing
+ * a forwarding unchecked exception.
  * 
  * @author Thomas Schneider
  * @version $Revision$
