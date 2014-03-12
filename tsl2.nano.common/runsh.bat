4498bb17c78ad093532864142cf294a953345185
Thomas Schneider <snieda@web.de>
2014-03-12 23:47:24 +0100

    run.bat --> runsh.bat

--- tsl2.nano.common/runsh.bat
+++ tsl2.nano.common/runsh.bat
@@ -3,13 +3,13 @@ echo ==========================================================================
 echo Windows ant-shell to start ant-scripts of shell.xml
 echo Author: Thomas Schneider / Thomas Schneider 2012
 echo help:
-echo   call 'run.bat -help' to see all ant helps and diagnostics.
+echo   call 'runsh.bat -help' to see all ant helps and diagnostics.
 echo example:
-echo   run.bat sh.java -Darg1=mypath.MyUnitTest
-echo   run.bat sh.java -Djava.classname=mypath.MyMainClass -Darg1=myArg1
-echo   run.bat sh.jar
-echo   run.bat sh.unjar
-echo   run.bat -prop my.properties sh.xml.xsl
+echo   runsh.bat sh.java -Darg1=mypath.MyUnitTest
+echo   runsh.bat sh.java -Djava.classname=mypath.MyMainClass -Darg1=myArg1
+echo   runsh.bat sh.jar
+echo   runsh.bat sh.unjar
+echo   runsh.bat -prop my.properties sh.xml.xsl
 echo ==========================================================================
 if "%1"=="-help" goto HELP
 if "%1"=="-?" goto HELP
