ed5a41434abd2c02a4eabaf65d17352ff5ebd67b
Thomas Schneider <snieda@web.de>
2014-03-23 13:17:29 +0100

    cleanup

--- tsl2.nano.h5/start.bat
+++
@@ -1,3 +0,0 @@
-set DEBUG="-agentlib:jdwp=transport=dt_socket,address=8787,server=y,suspend=y"
-java  -Djava.awt.headless=true %DEBUG% -jar tsl2.nano.h5.0.0.1.jar h5.sample 8070 >h5.sample/h5.sample.log
-pause
