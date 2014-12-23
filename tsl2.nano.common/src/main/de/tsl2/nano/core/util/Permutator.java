b4fc66f837055404c289009241974fecf49fbe2b
Thomas Schneider <snieda@web.de>
2014-12-23 18:40:53 +0100

    new: permutator, utilities on loops chg: crypt, print, netutil

--- tsl2.nano.common/src/main/de/tsl2/nano/core/util/Permutator.java
+++ tsl2.nano.common/src/main/de/tsl2/nano/core/util/Permutator.java
@@ -9,13 +9,25 @@
  */
 package de.tsl2.nano.core.util;
 
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.PipedInputStream;
 import java.io.PipedOutputStream;
 import java.io.PrintStream;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
 
+import de.tsl2.nano.collection.CollectionUtil;
+import de.tsl2.nano.core.Argumentator;
+import de.tsl2.nano.core.Finished;
 import de.tsl2.nano.core.ManagedException;
+import de.tsl2.nano.core.cls.BeanClass;
 
 /**
  * permutes the given array in the given range and prints to the given print stream. usable as brute force algorithm.
@@ -90,6 +102,90 @@ public class Permutator {
             for (char c = min; c <= max; c++) {
                 src[position] = c;
                 permute(out, src, position);
+            }
+        }
+    }
+
+    private static Map<String, String> getManual() {
+        HashMap<String, String> man = new LinkedHashMap<String, String>();
+        man.put("source", "(!) source collection");
+        man.put("transformer", "(!) transforming action");
+        man.put("swap", "(default:false) whether to swap key and values in destination-map");
+        man.put("backward", "(!) action to do a back-transformation for each keys value");
+        return man;
+    }
+
+    /**
+     * <pre>
+     * Source: collection of strings
+     * Transf: action to transform source-string
+     * swap  : whether to swap keys and values
+     * Dest  : property map with: source-string=transformation
+     * Trial : action on each dest-key, checking if action-result equals key-value
+     *       : distributing the work to many stations
+     * 
+     * </pre>
+     */
+    public static final void main(String[] args) {
+        Argumentator ator = new Argumentator("invader", getManual(), args);
+        Object t;
+        if (ator.check(System.out)) {
+            String source = ator.consume("source", "");
+            Collection src = CollectionUtil.load(source, null);
+            String transformer = ator.consume("transformer", "");
+            String mapname = source + ".transformed";
+            boolean swap = ator.hasOption("swap");
+
+            /*
+             * create a transformation map. if map was saved before,
+             * load it and check, if transformation was done.
+             * if not, append the new transformation to the map
+             */
+            Properties p = new Properties();
+            File fmap = new File(mapname);
+            if (fmap.exists())
+                try {
+                    p.load(new FileReader(fmap));
+                } catch (Exception e) {
+                    ManagedException.forward(e);
+                }
+            if (!p.values().containsAll(src)) {
+                transformer = transformer.replace("\"", "");
+                String[] a = transformer.split("\\s");
+                String[] a1 = new String[a.length - 2];
+                System.arraycopy(a, 2, a1, 0, a1.length);
+                for (Object o : src) {
+                    BeanClass bc = BeanClass.createBeanClass(a[0]);
+                    t = BeanClass.call(bc.getClazz(), a[1], false, a1);
+                    if (swap)
+                        p.put(t, o);
+                    else
+                        p.put(o, t);
+                }
+                FileUtil.saveProperties(mapname, p);
+            }
+            /*
+             * use the transormation map on the 'trial' action. check the 'trial' result 
+             * with the transformed result.
+             */
+            String backward = ator.consume("backward", "");
+            if (!Util.isEmpty(backward)) {
+                backward = backward.replace("\"", "");
+                String[] a = backward.split("\\s");
+                String[] a1 = CollectionUtil.copyOfRange(a, 2, a.length);
+                Set<Object> keySet = p.keySet();
+                BeanClass bc = BeanClass.createBeanClass(a[0]);
+                String v;
+                for (Object k : keySet) {
+                    v = (String) p.get(k);
+                    a1[0] = v;
+                    t = BeanClass.call(bc.getClazz(), a[1], false, a1);
+                    if (t != null && t.equals(k)) {
+//                        System.setProperty("invader.result", k + ":" + v);
+                        System.out.println(backward + " ==> " + k + ":" + v);
+                        Finished.apply();
+                    }
+                }
             }
         }
     }
