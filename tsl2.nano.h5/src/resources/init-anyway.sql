f363c77828dc6a3666364c4526bc29c4d6bf1b92
Thomas Schneider <snieda@web.de>
2015-05-24 13:12:49 +0200

    some testdata

---
+++ tsl2.nano.h5/src/resources/init-anyway.sql
@@ -0,0 +1,9 @@
+-- sample data for anyway database
+
+INSERT INTO "PUBLIC"."ORGANISATION" (ID,NAME,DESCRIPTION) VALUES (5,'timesheet','timesheet-system');
+INSERT INTO "PUBLIC"."ORGANISATION" (ID,NAME,DESCRIPTION) VALUES (7,'any','anything');
+
+INSERT INTO "PUBLIC"."ADDRESS" (ID,STREET,CODE,CITY,COUNTRY) VALUES (1,'Berliner Str.1','28376','Buxdehude','Deutschland');
+INSERT INTO "PUBLIC"."ADDRESS" (ID,STREET,CODE,CITY,COUNTRY) VALUES (2,'Frankfurter Strasse 376','60988','Berlin','Deutschland');
+INSERT INTO "PUBLIC"."ADDRESS" (ID,STREET,CODE,CITY,COUNTRY) VALUES (3,'Buxdehudener Strasse 5','10989','Hamburg','Deutschland');
+
