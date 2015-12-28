13593c29cbf30f3a667601364d1c97b6c4ef60e3
Thomas Schneider <snieda@web.de>
2015-12-28 23:47:18 +0100

    NEW: LogMeAs as AOP logger

---
+++ tsl2.nano.incubation/src/test/de/tsl2/nano/incubation/test/Test.java
@@ -0,0 +1,17 @@
+package de.tsl2.nano.incubation.test;
+
+import de.tsl2.nano.agent.AttachAgent;
+
+public class Test {
+  public int methodOne(int c) {
+    System.out.println("methodOne" + "(" + c + ")");
+    return c; 
+  }
+  public static void main(String[] args) {
+    AttachAgent.main(new String[0]);
+    Test t = new Test();
+    for (int i = 0; i < 10; i++) {
+      t.methodOne(i);
+    }
+  }
+}
