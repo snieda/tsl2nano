77a76cd40cbab61016644eefcc54dd77217c7bb2
Thomas Schneider <snieda@web.de>
2016-07-10 12:31:49 +0200

    RESTful attributes enhanced. NEW: runtime attribute definition NEW: broadcast messages to all sessions.

--- tsl2.nano.h5/src/main/de/tsl2/nano/h5/configuration/ExpressionDescriptor.java
+++ tsl2.nano.h5/src/main/de/tsl2/nano/h5/configuration/ExpressionDescriptor.java
@@ -12,22 +12,26 @@ package de.tsl2.nano.h5.configuration;
 import de.tsl2.nano.bean.BeanUtil;
 import de.tsl2.nano.bean.def.AbstractExpression;
 import de.tsl2.nano.core.cls.BeanClass;
+import de.tsl2.nano.core.util.NetUtil;
 
 /**
+ * describes an expression through its expression-pattern. tries to identifiy the type of expression and creates an
+ * instance of {@link AbstractExpression} through {@link #toInstance()}. provides some helpers to identify a string
+ * value (see {@link #isHtml(String)}, {@link #isURL(String)}, {@link #isJSON(String)}.
  * 
  * @author Tom, Thomas Schneider
- * @version $Revision$ 
+ * @version $Revision$
  */
 public class ExpressionDescriptor<T> extends AbstractExpression<T> {
 
     public ExpressionDescriptor() {
     }
-    
+
     /**
      * constructor
      */
     public ExpressionDescriptor(Class declaringClass) {
-        this.declaringClass = declaringClass; 
+        this.declaringClass = declaringClass;
     }
 
     @Override
@@ -42,18 +46,30 @@ public class ExpressionDescriptor<T> extends AbstractExpression<T> {
 
     @Override
     public void setValue(Object instance, T value) {
-        
+
     }
 
     public void setDeclaringClass(Class cls) {
         this.declaringClass = cls;
     }
-    
+
     @SuppressWarnings("rawtypes")
     AbstractExpression toInstance() {
         Class<? extends AbstractExpression> impl = AbstractExpression.getImplementation(getExpression());
         if (impl == null)
             throw new IllegalStateException("no implementation found for pattern: " + getExpression());
         return BeanUtil.copyValues(this, BeanClass.createInstance(impl));
+    }
+
+    public static boolean isHtml(String response) {
+        return response != null && response.contains("<html");
+    }
+
+    public static boolean isJSON(String response) {
+        return response != null && response.matches("\\{\\s*\\w+\\s*\\:\\w+\\,");
+    }
+
+    public static boolean isURL(String response) {
+        return response != null && NetUtil.isURL(response);
     }
 }
--- tsl2.nano.h5/src/main/de/tsl2/nano/h5/expression/RestfulExpression.java
+++ tsl2.nano.h5/src/main/de/tsl2/nano/h5/expression/RestfulExpression.java
@@ -16,16 +16,31 @@ import de.tsl2.nano.core.util.NetUtil;
 import de.tsl2.nano.execution.IPRunnable;
 
 /**
- * Usable as attribute getting it's value through a given restful service.
+ * Usable as attribute getting it's value through a given restful service. the expression is here an url query path. The
+ * default method is GET. If method is PUT or POST, the parent bean (this expression is part of an attribute!) will be
+ * transfered as JSON object.
  * 
  * @author Tom, Thomas Schneider
  * @version $Revision$
  */
 public class RestfulExpression<T extends Serializable> extends RunnableExpression<T> {
-    transient Object lastResult;
     /** serialVersionUID */
     private static final long serialVersionUID = -107140100937166501L;
 
+    transient Object lastResult;
+    /** REST method type */
+    String method = "GET";
+    /**
+     * if true, the url query will be created like: http://.../search?city=Munich&code=80000. otherwise, the parameter
+     * will be appended through '/'.
+     */
+    boolean urlQuerySeparators;
+    /**
+     * if true, the response-data will be embedded into the html (e.g. iframe-srcdoc) to be handled by a
+     * dependency-listener etc. - if false, the response is handled as link (like iframe-src)
+     */
+    boolean handleResponse;
+    
     static {
         registerExpression(RestfulExpression.class);
     }
@@ -51,22 +66,25 @@ public class RestfulExpression<T extends Serializable> extends RunnableExpressio
 
     @Override
     protected IPRunnable<T, Map<String, Object>> createRunnable() {
-        return new IPRunnable<T, Map<String,Object>>() {
+        return new IPRunnable<T, Map<String, Object>>() {
             @Override
             public T run(Map<String, Object> context, Object... extArgs) {
 //                if (!NetUtil.isOnline())
 //                    return (T) lastResult;
                 return (T) NetUtil.getRest(expression, extArgs);
             }
+
             @Override
             public String getName() {
                 return expression;
             }
+
             @Override
             public Map<String, ? extends Serializable> getParameter() {
                 // TODO Auto-generated method stub
                 return null;
             }
+
             @Override
             public Map<String, Object> checkedArguments(Map<String, Object> args, boolean strict) {
                 // TODO Auto-generated method stub
