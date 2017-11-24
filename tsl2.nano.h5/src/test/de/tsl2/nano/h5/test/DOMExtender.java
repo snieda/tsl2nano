9def5f41472fd7f0e4fbf018a8b2cb8903292a62
Thomas Schneider <snieda@web.de>
2017-11-24 14:54:02 +0100

    maven build: manifest-files moved, run-scripts changed for jar-names

---
+++ tsl2.nano.h5/src/test/de/tsl2/nano/h5/test/DOMExtender.java
@@ -0,0 +1,36 @@
+/*
+ * File: $HeadURL$
+ * Id  : $Id$
+ * 
+ * created by: Tom, Thomas Schneider
+ * created on: 22.11.2017
+ * 
+ * Copyright: (c) Thomas Schneider 2017, all rights reserved
+ */
+package de.tsl2.nano.h5.test;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+import de.tsl2.nano.core.ISession;
+import de.tsl2.nano.h5.IDOMExtender;
+
+/**
+ * 
+ * @author Tom, Thomas Schneider
+ * @version $Revision$ 
+ */
+public class DOMExtender implements IDOMExtender {
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void extend(Document doc, ISession<?> session) {
+        //TODO: create Testcase for that...
+        Element node = doc.createElement("h1");
+        node.setTextContent("Mein Test");
+       doc.getDocumentElement().appendChild(node);
+    }
+
+}
