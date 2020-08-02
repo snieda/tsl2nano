18c6bd676e8a99410691b7d38c10f17456c38938
Thomas Schneider <snieda@web.de>
2020-08-02 12:07:31 +0200

    run.sh +hprof

--- tsl2.nano.h5/src/resources/run.sh
+++ tsl2.nano.h5/src/resources/run.sh
@@ -45,6 +45,8 @@ JSU_ENC=-Dsun.jnu.encoding=UTF-8
 #DEBUG="-agentlib:jdwp=transport=dt_socket,address=localhost:8787,server=y,suspend=n"
 #AGENT=-javaagent:%PRJ%/aspectjweaver.jar
 #PROXY=-Dhttp.proxyHost=myproxy -Dhttp.proxyPort=8080
+#HPROF_CPU=-agentlib:hprof=cpu=samples
+#HPROF_HEAP=-agentlib:hprof=heap=dump
 #PROFILER="-agentpath:...visualvm_138/profiler/lib/deployed/jdk16/windows/profilerinterface.dll=...\visualvm_138\profiler\lib,5140"
 JAVA_OPTS="-Xmx512m -Djava.awt.headless=true"
 #LOG=">$PRJ.log"
@@ -54,6 +56,6 @@ IPv4="-Djava.net.preferIPv4Stack=true"
 #INTERNAL_DB=-Dapp.database.internal.server.run=true
 #TSL_SERVICE=-Dservice.url=https://tsl2-timesheet.herokuapp.com:5000
 java $IPv4 $OFFLINE $UH $LLANG $ENCODING $JSU_ENC $USERDIR $NANO_DEBUG $AGENT $PROXY  $DEBUG \
-	$UH $PROFILER $NO_DB_CHECK $NOSTARTPAGE $INTERNAL_DB $TSL_SERVICE \
+	$UH $HPROF_CPU $HPROF_HEAP $PROFILER $NO_DB_CHECK $NOSTARTPAGE $INTERNAL_DB $TSL_SERVICE \
 	$JAVA_OPTS -jar $NAME-$VERSION$EXTENSION.jar $PRJ $PORT $LOG 
 #if [ not "$NOPAUSE" == "nopause" ] then 'read -p' fi
