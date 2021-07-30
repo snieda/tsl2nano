a773800be59385a64fda69467692bab9e6716acb
Thomas Schneider <snieda@web.de>
2021-07-30 08:40:54 +0200

    autotest: h5 excludes nanoh5unit

--- tsl2.nano.h5/src/test/de/tsl2/nano/util/autotest/creator/AllAutoTests.java
+++ tsl2.nano.h5/src/test/de/tsl2/nano/util/autotest/creator/AllAutoTests.java
@@ -1,20 +1,26 @@
 package de.tsl2.nano.util.autotest.creator;
 
+import static de.tsl2.nano.autotest.creator.AutoTest.FILTER;
 import static de.tsl2.nano.autotest.creator.InitAllAutoTests.matchPackage;
+import static de.tsl2.nano.autotest.creator.InitAllAutoTests.set;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 import de.tsl2.nano.autotest.creator.AutoFunctionTest;
+import de.tsl2.nano.autotest.creator.AutoTest;
 import de.tsl2.nano.autotest.creator.CurrentStatePreservationTest;
 import de.tsl2.nano.autotest.creator.InitAllAutoTests;
+import de.tsl2.nano.core.util.StringUtil;
 import de.tsl2.nano.h5.NanoH5;
+import de.tsl2.nano.h5.NanoH5Unit;
 
 @RunWith(Suite.class)
 @SuiteClasses({InitAllAutoTests.class, AutoFunctionTest.class, CurrentStatePreservationTest.class})
 public class AllAutoTests {
 	public static void init() {
-		System.setProperty("tsl2.functiontest.filter", matchPackage(NanoH5.class));
+		set(FILTER, matchPackage(NanoH5.class));
+		set(AutoTest.FILTER_EXCLUDE, StringUtil.matchingOneOf(NanoH5Unit.class.getSimpleName()));
 	}
 }
