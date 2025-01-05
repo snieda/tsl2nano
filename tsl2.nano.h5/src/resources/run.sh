a420caabb74a551cd14a6d6965b0611d59d78cc8
Thomas Schneider <snieda@web.de>
2025-01-05 13:44:59 +0100

    doc

--- tsl2.nano.h5/src/resources/run.sh
+++ tsl2.nano.h5/src/resources/run.sh
@@ -14,8 +14,8 @@ SRC_FILE=mainargs.sh
 DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$DIR" ]]; then DIR="$PWD"; fi
 if [[ ! -f "$DIR/$SRC_FILE" ]]; then
-    curl https://github.com/snieda/tsl2-bash-scripts/raw/refs/heads/master/divers/mainargs.sh -o ~/.local/bin/$SRC_FILE && chmod +x .local/bin/$SRC_FILE
-    #(umask 755 && curl -O https://github.com/snieda/tsl2-bash-scripts/raw/refs/heads/master/divers/mainargs.sh)
+    curl -L https://github.com/snieda/tsl2-bash-scripts/raw/refs/heads/master/divers/mainargs.sh -o ~/.local/bin/$SRC_FILE && chmod +x .local/bin/$SRC_FILE
+    #(umask 755 && curl -LO https://github.com/snieda/tsl2-bash-scripts/raw/refs/heads/master/divers/mainargs.sh)
     #install -m 755 <( curl -Lf https://github.com/snieda/tsl2-bash-scripts/raw/refs/heads/master/divers/mainargs.sh) ~/.local/bin/$SRC_FILE
 fi
 . $DIR/mainargs.sh
